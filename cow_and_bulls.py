c6fcafbdb713212c57ac32bd29d3d0cb40d03ee6
Siiha <90766306+Siiha@users.noreply.github.com>
2025-02-21 15:09:02 +0200

    Add files via upload

--- cow_and_bulls.py
+++ cow_and_bulls.py
@@ -5,10 +5,17 @@ class Player:
 	name: str
 	guess: int=1
 n = input("Give your name: ")
+def input_check():
+	c = input("Code: ")
+	ch = lambda c: all([i in "0123456789" for i in c])
+	while len(c)!=4 or not ch(c) or len(c)!=len(set(c)):
+		print("The length of the code is 4 digits and all digits are unique.")
+		c = input("Code: ")
+	return c
 p1 = Player(n)
 def game(p):
 	code = "".join(str(i) for i in random.sample(range(0,9),4))
-	c = input("Code: ")
+	c = input_check()
 	def bulls_cows(c):a = sum([i==j for i,j in zip(code,c)]);b = sum([i in c for i in code])-a;return b,a
 		 
 	while code != c:
